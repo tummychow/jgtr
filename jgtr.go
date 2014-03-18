cad99ba2fccc94ee62bbd91f44e9cb6e460ef6c0
tummychow <tummychow@users.noreply.github.com>
2014-03-18 18:17:53 -0400

    Add help and version

--- jgtr.go
+++ jgtr.go
@@ -2,18 +2,68 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	flag "github.com/ogier/pflag"
 	"io/ioutil"
 	"os"
 	"text/template"
 )
 
+const helpStr = `    jgtr - JSON Go Template Renderer
+
+USAGE:
+    jgtr [OPTIONS]
+
+    jgtr consumes a JSON-encoded data file and a template file written in Go's
+    text/template language. The values available in the data file are then used
+    to render the template file and generate output.
+
+    By default, jgtr reads the data and template from stdin, and writes output
+    to stdout. Note that data and template cannot both come from stdin - at
+    least one of the two must be specified via an option.
+
+OPTIONS:
+    -j FILE, --json=FILE
+        Read JSON data from FILE. Specify "-" (the default) to use stdin.
+
+    -t FILE, --template=FILE
+        Read template from FILE. Specify "-" (the default) to use stdin.
+
+    -o FILE, --output=FILE
+        Write rendered template to FILE. Specify "-" (the default) to use
+        stdout.
+
+    -h, --help
+        Display this help.
+
+    -V, --version
+        Display jgtr version.`
+
+const versionStr = `0.0.0`
+
 func main() {
+	help := flag.BoolP("help", "h", false, "show help")
+	version := flag.BoolP("version", "V", false, "show version")
+
 	dataPath := flag.StringP("json", "j", "-", "JSON data file")
 	tmplPath := flag.StringP("template", "t", "-", "Go template file")
 	outPath := flag.StringP("output", "o", "-", "output file")
 
 	flag.Parse()
+
+	if *help {
+		fmt.Println(helpStr)
+		return
+	}
+	if *version {
+		println(versionStr)
+		return
+	}
+
+	if *dataPath == "-" && *tmplPath == "-" {
+		println("Cannot use stdin for data and template simultaneously")
+		os.Exit(1)
+	}
 
 	data, err := loadJSONData(*dataPath)
 	if err != nil {
