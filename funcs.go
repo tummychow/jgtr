d5e11ebb3ba6d6e5daa9f46cd848d2ce31c08aaf
tummychow <tummychow@users.noreply.github.com>
2014-03-22 03:27:15 -0400

    Add proper godoc comments for funcs.go

--- funcs.go
+++ funcs.go
@@ -9,6 +9,8 @@ import (
 	"time"
 )
 
+// tmplFuncs contains the function map for templates that will be rendered by
+// jgtr.
 var tmplFuncs = template.FuncMap{
 	"notLast": func(slice []interface{}, i int) bool {
 		return (i != len(slice)-1)
@@ -24,6 +26,8 @@ var tmplFuncs = template.FuncMap{
 	"stringLower":  strings.ToLower,
 	"stringTitle":  strings.ToTitle,
 
+	// sliceSort sorts a slice of empty interfaces, using the implementation
+	// provided by GenericSlice.
 	"sliceSort": func(s []interface{}) []interface{} {
 		ret := make([]interface{}, len(s))
 		copy(ret, s)
@@ -31,6 +35,8 @@ var tmplFuncs = template.FuncMap{
 		sort.Sort(GenericSlice(ret))
 		return ret
 	},
+	// sliceSortKey sorts a slice of empty interfaces on the given map key,
+	// using the implementation provided by MapSlice.
 	"sliceSortKey": func(s []interface{}, k string) []interface{} {
 		ret := make([]interface{}, len(s))
 		copy(ret, s)
@@ -38,6 +44,7 @@ var tmplFuncs = template.FuncMap{
 		sort.Stable(MapSlice{ret, k})
 		return ret
 	},
+	// sliceReverse reverses a slice of empty interfaces.
 	"sliceReverse": func(s []interface{}) []interface{} {
 		ret := make([]interface{}, len(s))
 		copy(ret, s)
@@ -49,28 +56,47 @@ var tmplFuncs = template.FuncMap{
 	},
 }
 
-// to implement functions of pkg sort on the generic slice,
-// we need to create a GenericSlice that implements sort.Interface
+// GenericSlice implements sort.Interface on a slice of interface{}, with the
+// assumption that all of those interface{} have the same underlying type, and
+// that type is some kind of int, float or string. The ordering that
+// GenericSlice imposes on those elements is the expected ordering (ie sorting
+// by ascending magnitude for ints/floats, and lexicographically for strings).
 type GenericSlice []interface{}
 
+// Less compares two elements of a GenericSlice. If the two elements are
+// comparable (ie they are both ints, uints, floats or strings), then Less will
+// return the result of a less-than comparison between those two elements. If
+// the two elements are not comparable (ie their types do not match, or they do
+// not have a natural ordering), then Less will panic.
 func (p GenericSlice) Less(i, j int) bool {
 	leftV := reflect.ValueOf(p[i])
 	rightV := reflect.ValueOf(p[j])
 	return valueLt(leftV, rightV)
 }
 
-// GenericSlice.Len and GenericSlice.Swap were stolen from standard implementations
-// in pkg sort
 func (p GenericSlice) Len() int      { return len(p) }
 func (p GenericSlice) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
 
-// another implementation of sort.Interface for slices of map[string]interface{}
-// Key is the key into the maps, against which they will be sorted
+// MapSlice implements sort.Interface on a slice of interface{}, with the
+// assumption that all of those interface{} have the underlying type
+// map[string]interface{}. The ordering that MapSlice imposes on those maps
+// is the standard ordering of a certain key, specified by Key. All maps in
+// Slice must either omit that key, or contain a value for that key, such
+// that all the maps contain the same type of value for that key.
 type MapSlice struct {
 	Slice []interface{}
 	Key   string
 }
 
+// Less compares two elements of a MapSlice. If both elements contain the key
+// specified by Key, then Less will return the result of comparing the values
+// associated with Key. If those values are of noncomparable types, Less will
+// panic.
+// If an element does not contain the key specified by Key, then it should sort
+// before the other element.
+// If both elements do not contain the key specified by Key, then they are
+// considered to be equal in ordering, so Less will return false.
+// If either element is not actually a map[string]interface{}, Less will panic.
 func (p MapSlice) Less(i, j int) bool {
 	// several layers of indirection in this code
 	// you have to reflect.ValueOf on the []interface{}, which reveals that the
@@ -99,7 +125,11 @@ func (p MapSlice) Less(i, j int) bool {
 func (p MapSlice) Len() int      { return len(p.Slice) }
 func (p MapSlice) Swap(i, j int) { p.Slice[i], p.Slice[j] = p.Slice[j], p.Slice[i] }
 
-// generic compare for pairs of ints, floats or strings
+// valueLT performs a less-than comparison on two reflect.Values. The Values
+// must both be valid receivers for one of the following reflection methods:
+// Value.Int, Value.Uint, Value.Float, or Value.String. This function will panic
+// if the Values do not have comparable underlying values, or if their Kinds
+// are not comparable in this way.
 func valueLt(leftV, rightV reflect.Value) bool {
 	switch leftV.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
